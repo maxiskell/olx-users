aa0e7e008291dc3acd2d7b528d59d50cd2e8fb7d
Maxi Schell <maximiliano.schell@smarttly.com>
2016-12-04 19:14:21 -0300

    Add response codes and exception handling

---
+++ app/Http/ResponseCodes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http;
+
+class ResponseCodes
+{
+    const HTTP_OK = 200;
+    const HTTP_BAD_REQUEST = 400;
+    const HTTP_NOT_FOUND = 404;
+    const HTTP_METHOD_NOT_ALLOWED = 405;
+    const HTTP_INTERNAL_SERVER_ERROR = 500;
+}
--- public/index.php
+++ public/index.php
@@ -1,11 +1,31 @@
 <?php
 
+namespace App;
+
+use App\Http\ResponseCodes;
+
 require __DIR__ . '/../bootstrap/autoload.php';
 require __DIR__ . '/../bootstrap/database.php';
 
 $app = require __DIR__ . '/../bootstrap/app.php';
 
-$response = $app->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+try {
+    $response = $app->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    $code = ResponseCodes::HTTP_OK;
+} catch (\Phroute\Phroute\Exception\HttpRouteNotFoundException $e) {
+    $code = ResponseCodes::HTTP_NOT_FOUND;
+    $response = '404 - Not Found';
+} catch (\Phroute\Phroute\Exception\HttpMethodNotAllowedException $e) {
+    $code = ResponseCodes::HTTP_METHOD_NOT_ALLOWED;
+    $response = '405 - Method Not Allowed';
+} catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+    $code = ResponseCodes::HTTP_NOT_FOUND;
+    $response = '404 - Not Found';
+} catch (Exception $e) {
+    $code = ResponseCodes::HTTP_INTERNAL_SERVER_ERROR;
+    $response = '500 - Internal Server Error';
+}
 
 header('Content-Type: application/json');
+http_response_code($code);
 echo json_encode($response);
